74cdb77d080d3468684d8b7f01cf2c742d8b3cf9
Ales2003 <ales_2003@mail.ru>
2016-03-18 17:54:57 +0300

    First commit

---
+++ src/filestorage/AbstractFile.java
@@ -0,0 +1,21 @@
+
+package filestorage;
+
+public abstract class AbstractFile implements InterfaceFile {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    
+            
+   
+    
+    
+}
